fd5c073d75a40ccc367b67f562f41fc111c7fc5d
Simone <simone.robutti@protonmail.com>
2023-12-01 23:42:15 +0100

    functional refactoring

--- src/days/1/Puzzle.ts
+++ src/days/1/Puzzle.ts
@@ -1,24 +1,33 @@
-const findDigit = (s: string) => {
-  for (let i of s) {
-    const n = Number(i)
-    if (!isNaN(n)) { return n }
-  }
-}
-const extractNumber = (s: string) => {
-  return findDigit(s) * 10 + findDigit(s.trim().split("").reverse().join(""))
+type findFunction = (s: string) => number
+
+
+const firstLetterDigitMatcher = /(\d|one|two|three|four|five|six|seven|eight|nine)/;
+const lastLetterDigitMatcher = /(\d|enin|thgie|neves|xis|evif|ruof|eerht|owt|eno)/;
+const findDigitMatcher = /^[^\d]*(\d)\w*/i
+
+const findDigit = (reg: RegExp) => (s: string) => parseInt(reg.exec(s)[1])
+
+const extractDigits = (findFunc: findFunction, findFuncRev: findFunction) => (s: string) => findFunc(s) * 10 +
+  findFuncRev(s.trim().split("").reverse().join(""))
+
+
+const findAndSumDigits = (input: string, findFunc: findFunction, findFuncRev: findFunction) => {
+  return input.split("\n").map(extractDigits(findFunc, findFuncRev)).reduce(
+    (a, c) => a + c, 0
+  )
 }
 
 const first = (input: string) => {
-  return input.split("\n").map(extractNumber).reduce(
-    (a, c) => a + c, 0
-  )
+  return findAndSumDigits(input, findDigit(findDigitMatcher), findDigit(findDigitMatcher))
 };
 
 const expectedFirstSolution = 'solution 1';
 
+
+
 const second = (input: string) => {
 
-  return 'solution 2';
+  return findAndSumDigits(input,)
 };
 
 const expectedSecondSolution = 'solution 2';
