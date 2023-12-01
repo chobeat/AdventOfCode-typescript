43e36a113021fe8708aed33af3fe40a28126d2c3
Simone <simone.robutti@protonmail.com>
2023-12-01 19:18:58 +0100

    implemented solution to Day1-1

--- src/days/1/Puzzle.ts
+++ src/days/1/Puzzle.ts
@@ -1,12 +1,23 @@
+const findDigit = (s: string) => {
+  for (let i of s) {
+    const n = Number(i)
+    if (!isNaN(n)) { return n }
+  }
+}
+const extractNumber = (s: string) => {
+  return findDigit(s) * 10 + findDigit(s.trim().split("").reverse().join(""))
+}
+
 const first = (input: string) => {
-  console.log(input);
-  return 'solution 1';
+  return input.split("\n").map(extractNumber).reduce(
+    (a, c) => a + c, 0
+  )
 };
 
 const expectedFirstSolution = 'solution 1';
 
 const second = (input: string) => {
-  console.log(input);
+
   return 'solution 2';
 };
 
